6dbe44827fe23bd158874637f174cd462877328b
Balaji380 <balaji.p2022eee@sece.ac.in>
2025-11-02 12:09:04 +0530

    Init-database

---
+++ src/model/user.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose")
+
+const userSchema=mongoose.Schema(
+    {
+        firstName:{
+            type:String
+        },
+        email:{
+            type:String
+        },
+        age:{
+            type:Number
+        }
+    }
+)
+
+
+module.exports=mongoose.model("User",userSchema)
--- src/server.js
+++ src/server.js
@@ -1,7 +1,43 @@
 const express=require("express")
 const app=express();
+const connectDB=require("./config/database")
+const User=require("./model/user")
 
 
-app.listen(3000,(req,res)=>{
-    console.log("Server is running at port 3000")
+
+app.post("/signup",async(req,res)=>{
+
+    const userObj={
+        firstName:"Balaji",
+        email:"balu@gmail.com",
+        age:20
+    }
+
+    const user=new User(userObj);
+   
+    try {
+        await user.save()
+       res.send("User saved successfully")
+        
+    } catch (error) {
+        res.status(500).send("Error saving User data")
+    }
+    
+
 })
+
+
+
+
+connectDB()
+    .then(()=>{
+     console.log("Database connected successfully")
+    app.listen(3000,(req,res)=>{
+    console.log("Server is running at port 3000")
+    })
+   })
+   .catch((err)=>{
+     console.log("Database cannot be connected")
+   })
+
+
